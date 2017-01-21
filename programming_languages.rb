d26c423f676a95637ecb7d293ca169ff92b67667
jason brown <j.grant.brown@gmail.com>
2017-01-21 02:08:24 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,3 +1,16 @@
 def reformat_languages(languages)
   # your code here
 end
+
+=begin first attempt
+def reformat_languages(languages)
+  # your code here
+ new_hash={}
+ style=[ ]
+languages.map{|style,values|
+ values.map{|lang,type| new_hash[lang]={ style: "#{style}"}
+
+  }}
+new_hash
+end
+=end
