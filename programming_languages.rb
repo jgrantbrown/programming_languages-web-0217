346ee4d720224005f0a558cc5a754f5dd1529133
jason brown <j.grant.brown@gmail.com>
2017-01-23 22:23:38 +0000

    Done.

--- programming_languages.rb
+++ programming_languages.rb
@@ -1,7 +1,18 @@
 def reformat_languages(languages)
   # your code here
-end
+ new_hash={}
+    languages.map{|style,values|
+    values.map{|lang,type|  if !new_hash.key?(lang)
+              new_hash[lang]=type
+              new_hash[lang][:style]=[style]
+            else new_hash[lang][:style]<<style
+ end
 
+  }
+  }
+new_hash
+end
+# thanks for a walkthrough from dylatino2
 =begin first attempt
 def reformat_languages(languages)
   # your code here
